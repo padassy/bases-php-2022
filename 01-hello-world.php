d158a259ad825d08dfcba3ee578f29fb636ed4f5
mikhawa <michaeljpitz@gmail.com>
2022-10-05 11:49:09 +0200

    hello world

---
+++ 01-hello-world.php
@@ -0,0 +1,23 @@
+<?php
+
+// commentaire sur une ligne
+
+// on peut mettre des balises PHP n'importe où dans une page html
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+    <?php // ouverture de PHP
+
+    // affiche la ligne : echo "text" ; 
+    echo "<h1>Hello World</h1>";
+   // fermeture de PHP ?>
+</body>
+</html>
