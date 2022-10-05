80218e877d238527428ac127ff1d09dc250d41cb
padassy <pierre-alain.dassy@cf2000.onmicrosoft.com>
2022-10-05 11:48:07 +0200

    commit1

---
+++ 01-hello-world.php
@@ -0,0 +1,20 @@
+<?php
+// commentaire sur une ligne 
+// on peut mettre des balises php n'importe ou dans une page html 
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+    <?php // ouverture php
+    //Affiche la ligne : echo "text"
+echo "<h1>Hello World</h1>";
+
+   // fermeture de php  ?>
+</body>
+</html>
