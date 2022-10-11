0d8d81a85d232240c53f3708a02682c615838f27
mikhawa <michaeljpitz@gmail.com>
2022-10-11 14:25:10 +0200

    folder

---
+++ 07-foreach.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ le concept de boucle est important dans tous les langages, en PHP il existe diverses manières de créer des boucles :
+
+ foreach => boucles sur les tableaux et objets
+ for => boucles de type numériques
+ while => boucle de vérification true
+ do while => boucle de vérification true avec 1 exécution obligatoire
+ fonctions récursives => Vous créez vos propres boucles ! 
+
+ Les boucles ne doivent jamais être infinies.
+
+ */
