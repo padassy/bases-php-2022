dedb66dc7fc806a5a979e963b336b393e2724f24
mikhawa <michaeljpitz@gmail.com>
2022-10-14 13:48:38 +0200

    elseif

--- 12-conditions.php
+++ 12-conditions.php
@@ -29,5 +29,30 @@ if(isset($nombre)&& $nombre>5 && $nombre < 15){
     echo "$nombre existe ET est plus grand que 5 ET est plus petit que 15";
 }
 ?>
+<h2>else - SINON</h2>
+    <p>C'est l'exécution de code si jamais le if qui précède est à false</p>
+<?php
+$temp = mt_rand(-20,20);
+echo "<h4>Il fait $temp ° dehors</h4>";
+if($temp <= 0){
+    echo "Il gèle dehors à $temp °!";
+}else{
+    echo "Il ne gèle pas pour le moment";
+}
+?>
+<h2>elseif - else if - SINON SI</h2>
+    <p>C'est l'exécution d'une vérification supplémentaire si jamais le if (ou autre elseif) qui précède est à false</p>
+<?php
+if($temp <= -10){
+    echo "Il fait très froid dehors.";
+}elseif($temp <=0){
+    echo "Il fait froid dehors.";
+}elseif($temp <=12){
+    echo "Il fait frisquet.";
+// dans les autres cas (default)    
+}else{
+    echo "Il fait agréable dehors.";
+}
+?>
 </body>
 </html>
