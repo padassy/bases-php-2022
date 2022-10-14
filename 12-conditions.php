92de5a10943862d3da2bad457c90d16c79598560
padassy <pierre-alain.dassy@cf2000.onmicrosoft.com>
2022-10-14 16:20:50 +0200

    commit

--- 12-conditions.php
+++ 12-conditions.php
@@ -7,6 +7,7 @@
     <title>Document</title>
 </head>
 <body>
+    
     <h1>Les conditions</h1>
     <h2>if (conditions), Si</h2>
     <p>Plus basiques que les boucles, les conditions sont cependants la base de tous les languages de programmation</p>
@@ -22,7 +23,47 @@
     if ($nombre<10) {
         echo "$nombre est plus petit que 10";
     } 
+    // || est true si 1 true minimum
+    // XOR ^ est true si 1 true maximum
+    //&& est true si tout est true 
+    // ! inversion entre true et false.. false devient true et vice-versa
     ?>
     <p>Une condition peut etre plus complexe  et donc utiliser les && (et), || (ou), ! (contraire de) et XOR (^ ou stricte) </p>
+    <?php
+    // si la variable est initialisée (isset) ici $nombre existe 
+    if (isset($nombre)&& $nombre>5 && $nombre< 15){
+        echo "$nombre existe Et est plus grand que 5 Et plus petit que 15";
+    }
+    ?>
+    <h2>else- Sinon</h2>
+    <p>C'est l'exécution du code si jamais le if qui précède est false</p>
+    <?php
+    $temp =mt_rand (-20,20);
+    echo "<h4>$temp ° dehors </h4>";
+    if ($temp <= 0 ){
+        echo "Attention il gèle à  $temp °! ";
+    }
+    else {
+        echo "Il fait bon, t'inquiète ";
+    }
+    ?>
+
+    <h2> else if- elseif - Sinon si </h2>
+    <p>C'est l'exécution d'une vérification supplémentaire si jamais le if ou autre else if qui précède est false </p>
+    <?php
+    if ($temp <=-10){
+        echo "il fait très froid";
+    }
+    else if ($temp <=0){
+        echo "il fait juste froid";
+    }
+    elseif ($temp <=12){
+        echo "Il fait frisquet";
+    }
+    else {
+        echo "Il fait bon frère";
+    }
+
+    ?>
 </body>
 </html>
---
+++ interface-utilisateur-if.php
@@ -0,0 +1,39 @@
+<?php
+var_dump($_POST);
+// Débigage de la variable POST
+// si il existe la variable $_POST nommée temperature - Dans notre cas $_POST existe que si on a envoyé le formulaire
+if (isset($_POST['temperature'])){
+    echo "ok";}
+    // on va créer une variable temporaire locale et changer le typage de notre variable en entier en utilisant (int) pour integer, si ca fonctionne ca nous donne un numérique, en cas d'echec ca nous donne le numérique 0
+    $temp = (int) $_POST['temperature'];
+    var_dump($temp);
+    if ($temp ===0){/*$to      = 'padassy@gmail.com';
+        $subject = 'Message du pape';
+        $message = 'Tu as été touché par la grace du dieu ';
+        $headers = 'From: francois@vatican.va' . "\r\n" .
+            'Reply-To: francois@vatican.va' . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+        
+        mail($to, $subject, $message, $headers);
+        //mail ("padassy@gmail.com", "Salut couillon", "Voici un message du pape", "from:francois@vatican.va". "\r\n" .'Reply-To: francois@vatican.va' . "\r\n" .'X-Mailer: PHP/' . phpversion();)
+        //header("Location :https://cdn4.vectorstock.com/i/1000x1000/13/93/no-stupid-people-beyond-this-point-vintage-rusty-vector-35281393.jpg");
+        //exit ();*/
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Températures</title>
+</head>
+<body>
+    <h1>Températures</h1>
+    <form name="name1" method="POST" action="">
+        <p>Bonjour, quel est la température dehors?</p>
+        <input type="number" name="temperature" placeholder="Mettez la temperature" min="-100" max="100">
+        <input type="submit" value="Envoyez">
+    </form>
+</body>
+</html>
