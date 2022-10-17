c2d0164e3ab0f576e07f27120078c1508df6e141
mikhawa <michaeljpitz@gmail.com>
2022-10-17 10:19:36 +0200

    correction 11

---
+++ 11-boucles/11-exe-boucles-correction.php
@@ -0,0 +1,122 @@
+<?php
+$stagiaires=[
+    "Redouan",
+    "Coralie",
+    "Florence",
+    "Tom",
+    "Billy",
+    "Jaouad",
+    "Frédéric",
+    "Maksym",
+    "Magib",
+    "Pierre-Alain",
+    "Youssef",
+    "Jonathan",
+    "Melvin",
+    "Yanis",
+    "Robbins",
+    "Ana-Maria",
+    "Jennifer",
+    "Valérie",
+];
+
+// chiffre au hasard de 0 à 17
+$hasard = mt_rand(0,17);
+
+$nb1 = -100;
+
+$nb2 = 0;
+
+$nb3 = 100;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boucles correction</title>
+</head>
+<body>
+    <h1>Exercices sur les boucles</h1>
+    <p>Créez dans ce dossier un fichier exe-11-{prénom}.php en copiant celui-ci</p>
+
+    <h3>Exe 1</h3>
+    <p>Affichez tous les noms de la variable $stagiaires séparés par un " | "</p>
+    <?php
+    foreach($stagiaires as $valeur){
+        echo "$valeur | ";
+    }
+    ?>
+    <h3>Exe 2</h3>
+    <p>Affichez tous les noms de la variable $stagiaires séparés par un " | " avec leur clef: exemple <code>0 -> Redouan | 1 -> Coralie ...</code></p>
+    <?php
+    foreach($stagiaires as $clef => $valeur){
+        echo "$clef -> $valeur | ";
+        //echo $clef . " \"-> " . $valeur . ' " \'| ';
+    }
+    ?>
+    <h3>Exe 3</h3>
+    <p>Voir la doc sur les tableaux : Affichez tous les noms de la variable $stagiaires classés par ordre alphabétique séparés par un " | "</p>
+    <p>Voici le var_dump de la variable $stagiaires avant le tri :</p>
+    <?php
+    var_dump($stagiaires);
+    // Le = permet de faire une copie exacte de $stagiaires dans une autre variable (fonctionne pour toutes les variables, les objets faisant exceptions, il faudra utiliser clone pour ceux ceux-ci)
+    $tab_asort = $stagiaires;
+    $tab_sort = $stagiaires;
+    // tri en gardant les clef
+    asort($tab_asort);
+    // tri en recréant les clef (indexé)
+    sort($tab_sort);
+
+    echo "<h4>sort -> on perd le lien avec la clef</h4>";
+    // exercice - on peut utiliser ou sort, ou asort
+    foreach($tab_sort as $item){
+        echo "$item | ";
+    }
+    print "<hr>";
+    // on va voir la différence entre asort et sort en affichant la clef
+
+    foreach($tab_sort as $key => $item){
+        echo "$key -> $item | ";
+    }
+    ?>
+    <h4>asort -> on garde le lien avec la clef</h4>
+    <?php
+    foreach($tab_asort as $key => $item):
+        echo "$key -> $item | ";
+    endforeach;
+    ?>
+
+    <h4>Exe 4 </h4>
+    <p>Affichez les chiffres de 0 à la valeur de la variable $hasard (entiers positif : 0, 1, 2, 3, ...) séparés par des ,</p>
+    <?php
+    $nb = 0;
+    while($nb<=$hasard){
+        echo "$nb, ";
+        $nb++;
+    }
+    ?><hr>
+    <?php
+    for($i=0;$i<=$hasard;$i++){
+        echo "$i, ";
+    }
+    ?>
+    <h4>Exe 5 </h4>
+    <p>faites une boucle allant de $nb3 à $nb1 en diminuant la valeur de 3 (100, 97, 94 etc...) séparés par des virgules</p>
+    <?php
+    $i=3;
+    while($nb3>=$nb1){
+        echo "$nb3, ";
+        $nb3-=$i;
+    }
+    ?><hr>
+    <?php
+    $nb3 = 100; $nb1 = -100;
+    for($i=$nb3;$i>=$nb1;$i-=3){
+        echo "$i, ";
+    }
+    ?>
+</body>
+</html>
--- 13-interface-utilisateur-if.php
+++ 13-interface-utilisateur-if.php
@@ -1,5 +1,5 @@
 <?php
-//débogage de la variable POST
+//débogage de la variable superglobale POST
 var_dump($_POST);
 
 // si il existe la variable $_POST nommée temperature (utilisation de isset) - Dans notre cas? $_POST['temperature'] existe si on a envoyé le formulaire
@@ -8,6 +8,7 @@ if(isset($_POST['temperature'])){
     // on va créer une variable temporaire locale, et changer le typage de notre variable en entier en utilisant (int) pour integer, si ça fonctionne ça nous donne un numérique, en cas d'échec ça nous donne le numérique 0
     $temp = (int) $_POST['temperature'];
     var_dump($temp);
+    /*
     if($temp===0){
         //mail("michael.pitz@cf2m.be","Je vous regarde !","Corinne, Voici un message du pape: Que Dieu vous bénisse !",'From: francois@vatican.va' . "\r\n" .
         //'Reply-To: francois@vatican.va' . "\r\n" .
@@ -15,6 +16,7 @@ if(isset($_POST['temperature'])){
         header("Location: https://www.topito.com/video/top-pires-photos-sites-rencontre-russes-putin-beau-gosse");
         exit();
     }
+    */
 }
 
 ?>
