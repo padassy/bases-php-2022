2f9194575a0c3fb1f9fdf0101fb6c03879c8b470
Maksym Sokol <112185430+Maksym995@users.noreply.github.com>
2022-10-19 09:05:34 +0200

    exe14

---
+++ 14-exe/14-calculatrice-maksym .php
@@ -0,0 +1,60 @@
+<?php
+if(isset($_POST['first'],$_POST['choix'],$_POST['second'],)){
+    // exemple si on a envoyé le formulaire
+    $reponse ="formulaire envoyé";
+
+    // vérification des champs (!! on ne peut pas diviser par 0)
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculatrice</title>
+</head>
+<body>
+    <h1>Calculatrice</h1>
+    <h2>Exercice calculatrice</h2>
+    <p>Créez une branche et un nouveau fichier dans 14-exe/14-calcul-{prénom}.php<br>
+    Envoyez-moi le git pull request sur notre dépôt de la classe</p>
+    <p>$reponse doit contenir la réponse au calcul, ou l'erreur de calcul ou de fonctionnement</p>
+    <form action="" name="calcul" method="POST">
+        <input type="text" name="first" />
+        <select name="choix">
+            <option value="addition">+</option>
+            <option value="soustraction">-</option>
+            <option value="multiplication">*</option>
+            <option value="division">/</option>
+        </select>
+        <input type="text" name="second" />
+        <input type="submit" value="Calculer"/>
+    </form>
+    <?php
+        if (isset($choix)){
+            echo("Resultat du calcul ".$first.$choix.$second." : ");
+    
+            if ($choix=="+" && is_numeric($first + $second)) {
+                echo "$first + $second";
+            } elseif ($choix == "-" && is_numeric($first - $second)) {
+                echo "$first - $second";
+            } elseif ($choix == "*" && is_numeric($first * $second)) {
+                echo "$first * $second";
+            } elseif ($second == 0 && is_numeric($first / $second))
+                echo "Vous ne pouvez pas diviser par 0";
+            else {
+                echo "$first / $second";
+            }
+        }
+
+    if(isset($reponse)):
+        echo $reponse;
+    endif;
+    ?>
+    <hr>
+    <?php
+    var_dump($_POST);
+    ?>
+</body>
+</html>
