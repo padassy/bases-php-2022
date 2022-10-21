9b39d593b5bf23c6611b5c05520d4d54d70a888c
padassy <pierre-alain.dassy@cf2000.onmicrosoft.com>
2022-10-21 16:07:58 +0200

    commit

---
+++ 19-cf/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Bare - Start Bootstrap Template</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
+<?php
+
+// Appel des dépendances 
+require_once "config.php";
+
+// si il existe une variable GET nommée page 
+if (isset($_GET['page'])){
+    echo"yes";
+// sinon
+}else{
+    // inclusion d'homepage une seule fois
+    include_once "homepage.php";
+}
+
+?>
