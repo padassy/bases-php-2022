6f3353dfe71a6f56c065e784585336afe665fcb1
mikhawa <michaeljpitz@gmail.com>
2022-10-26 14:25:20 +0200

    structure

---
+++ 20-cf-exe/pages/homepage.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil 20</title>
+    <link rel="stylesheet" href="css./myCSS.css">
+</head>
+<body>
+    <h1>Accueil 20</h1>
+    <ul class="nav">
+        <li><a href="./">Accueil 20</a>
+        <li><a href="?u=formateurs">Formateurs</a></li>
+        <li><a href="?u=stagiaires">Stagiaires</a></li>
+    </ul>
+</body>
+</html>
