ca92201f2f8defb490be7a283c7b3e252f08a8d9
marshmalocode <gustinmelvin6@gmail.com>
2022-10-27 16:11:56 +0200

    exe20

---
+++ 20-cf-exe-melvin/index.php
@@ -0,0 +1,23 @@
+<?php
+
+include "./pages/navbar.php";
+
+require_once "configuration.php";
+if(isset($_GET['u'])){
+       switch($_GET['u']){
+        case 'classes':
+            include "pages/classcf.php";
+            break;
+        case 'formateurs':
+            include 'pages/formaweb.php';
+            break;
+        case 'stagiaires':
+            include 'pages/stagweb.php';
+            break;
+        default:
+            include_once "pages/homepage.php";
+    } 
+}else{
+    include_once "pages/homepage.php";
+};
+   include "./pages/footer.php";
