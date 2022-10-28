b3d3ea17b6797b4b9d6ab3a0aa5d752910573f0e
padassy <pierre-alain.dassy@cf2000.onmicrosoft.com>
2022-10-28 09:32:41 +0200

    correction

---
+++ 20-cf-exe-p-a/pages/footer.php
@@ -0,0 +1,5 @@
+<footer class="navbar navbar-expand-lg navbar-dark bg-dark fixed-bottom">
+            <div class="container-fluid">
+                <p class="text-light position-absolute top-50 start-50 translate-middle-y">CF2m &copy; <?=date("Y") ?></p>
+            </div>
+        </footer>
