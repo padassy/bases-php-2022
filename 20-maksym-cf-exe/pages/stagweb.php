42cf567890a1e0de61514ba3d44eea1eba867743
Maksym Sokol <112185430+Maksym995@users.noreply.github.com>
2022-10-28 09:26:24 +0200

    exe20m

---
+++ 20-maksym-cf-exe/pages/stagweb.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stagiaires WEB</title>
+    <link rel="stylesheet" href="css/myCSS.css">
+</head>
+<body>
+    <h1>Stagiaires WEB</h1>
+        <?php
+        include "menu.php";
+        ?>
+    <container>
+        <h2>Liste Stagiaires WEB</h2>
+        <?php
+        foreach(STAGIAIRES_WEB AS $stagiaires):
+        ?>
+        <ul>
+            <li><?=$stagiaires[0]?></li>
+            <li><?=$stagiaires[1]?></li>
+        </ul>
+        <?php
+        endforeach;
+        ?>
+    </container>
+        <?php 
+        include "footer.php" 
+        ?>
+</body>
+</html>
