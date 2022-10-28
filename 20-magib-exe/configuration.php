8f92d6329ca4f2819e064b44d6267e9fa96bcb83
Magib <112185392+mageeeb@users.noreply.github.com>
2022-10-28 11:55:03 +0200

    exe-20-magib-okkkkk

---
+++ 20-magib-exe/configuration.php
@@ -0,0 +1,38 @@
+<?php
+
+const FORMATEURS_WEB = [
+    ["Pierre", "Sandron", "HTML"],
+    ["André", "Palmisano", "JS"],
+    ["Michaël","Pitz","PHP"],
+];
+
+const STAGIAIRES_WEB = [
+    ["Bik","Tom"],
+    ["Bugara","Jennifer"],
+    ["Dassy","Pierre-Alain"],
+    ["El Berkani","Jaouad"],
+    ["Gustin","Melvin"],
+    ["Haddad M Saouri","Redouan"],
+    ["Jaymes","Florence"],
+    ["Karkouch","Youssef"],
+    ["Lemfadli","Yanis"],
+    ["Rauzy","Jonathan"],
+    ["Sall","Magib"],
+    ["Sira","Coralie"],
+    ["Sokol","Maksym"],
+    ["Tabonkeng","Billy"],
+    ["Tîrdea","Ana-Maria"],
+    ["Van Huffle","Valérie"],
+    ["Vermeulen","Frédéric"],
+    ["Wamba","Cyrille Robbins"],
+];
+
+const CLASS_CF2M = [
+    "Animateur multimédia",
+    "Atouts jeunes",
+    "Aventure digitale",
+    "Digital designer",
+    "Web Developer FullStack",
+    "Technicien support PC et réseaux",
+];
+
---
+++ 20-magib-exe/index.php
@@ -0,0 +1,26 @@
+
+<?php
+require_once "configuration.php";
+if (isset($_GET['u'])) {
+    switch ($_GET['u']) {
+        case 'classes':
+            include "pages/classcf.php";
+            break;
+        case 'formateurs':
+            include 'pages/formaweb.php';
+            break;
+        case 'stagiaires':
+            include 'pages/stagweb.php';
+            break;
+        default:
+            include_once "pages/homepage.php";
+    }
+} else {
+    include_once "pages/homepage.php";
+};
+
+
+  include "footer.php"
+
+    ?>
+ 
