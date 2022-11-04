1ab4c7f02783d2fd36f60609f37808342b397669
mikhawa <michaeljpitz@gmail.com>
2022-11-04 11:03:53 +0100

    22

--- 22-functions.php
+++ 22-functions.php
@@ -21,3 +21,25 @@ function calculBasic($num1, $num2, $op = "+"){
             return $num1+$num2;
     }
 }
+
+function calculBasicWithInstruction($num1, $num2, $op = "+"){
+    // on inverse la condition avec !, attention le && devient ||
+    if(!is_numeric($num1)||!is_numeric($num2)){
+        return "Attention, les 2 premiers paramètres doivent être numériques";
+    }
+    switch ($op){
+        case "*":
+            return $num1." ".$op." ".$num2." = ".($num1*$num2); // le return arrête la fonction et renvoie le résultat
+            break; // break facultatif dans ce cas particulier
+        case "-":
+            return $num1." ".$op." ".$num2." = ".($num1-$num2);
+            break;
+        case "/":
+            // si division par 0 affichage de l'erreur et arrêt du script
+            if($num2==0) return "Division par 0 interdite";
+            return $num1." ".$op." ".$num2." = ".($num1/$num2);
+            break;
+        default:
+            return "$num1 $op $num2 = ".($num1+$num2);
+    }
+}
