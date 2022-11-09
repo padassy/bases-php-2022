a9999a3ea4a87823275a162bd6448cb549e7f481
mikhawa <michaeljpitz@gmail.com>
2022-11-09 08:42:25 +0100

    config.php

--- 23-securisation/config.php
+++ 23-securisation/config.php
@@ -4,3 +4,39 @@
 
 const EMAIL_PROPRIO = "michaeljpitz@gmail.com";
 const RESULTATS_PG = 5;
+
+const FORMATEURS_WEB = [
+    ["Pierre", "Sandron", "HTML"],
+    ["André", "Palmisano", "JS"],
+    ["Michaël", "Pitz", "PHP"],
+];
+
+const STAGIAIRES_WEB = [
+    ["Bik", "Tom"],
+    ["Bugara", "Jennifer"],
+    ["Dassy", "Pierre-Alain"],
+    ["El Berkani", "Jaouad"],
+    ["Gustin", "Melvin"],
+    ["Haddad M Saouri", "Redouan"],
+    ["Jaymes", "Florence"],
+    ["Karkouch", "Youssef"],
+    ["Lemfadli", "Yanis"],
+    ["Rauzy", "Jonathan"],
+    ["Sall", "Magib"],
+    ["Sira", "Coralie"],
+    ["Sokol", "Maksym"],
+    ["Tabonkeng", "Billy"],
+    ["Tîrdea", "Ana-Maria"],
+    ["Van Huffle", "Valérie"],
+    ["Vermeulen", "Frédéric"],
+    ["Wamba", "Cyrille Robbins"],
+];
+
+const CLASS_CF2M = [
+    "Animateur multimédia",
+    "Atouts jeunes",
+    "Aventure digitale",
+    "Digital designer",
+    "Web Developer FullStack",
+    "Technicien support PC et réseaux",
+];
