40333e92c730fca85f30391c6f0daeb34797d62c
JonRauzy <jonathan.rauzy@gmail.com>
2022-11-14 16:29:12 +0100

    ex025.1

---
+++ 25-jon-boston/pg/bagarre.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BOSTON BEATDOWN</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <?php include 'header.php' ?>
+    <main>
+        <h2>BAGARRE</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab doloremque assumenda praesentium, aspernatur autem consequuntur sed optio delectus magni sint sapiente incidunt fuga numquam error quis. Dolore earum perspiciatis quos.</p>
+    </main>
+
+</body>
+
+</html>
