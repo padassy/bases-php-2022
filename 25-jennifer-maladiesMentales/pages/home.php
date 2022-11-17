12aa921fe591a0b30f6ec16421865fd1e1ec7e23
jenniferboo <bugara.jennifer@gmail.com>
2022-11-17 11:58:28 +0100

    exejen

---
+++ 25-jennifer-maladiesMentales/pages/home.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>home</title>
+</head>
+<body> 
+    <?php include 'incl-nav.php'?>
+    <h1>maladies mentales</h1>
+    <p>Les malades mentaux ne savent pas qu'ils sont malades. Néanmoins, ils peuvent reconnaître l'histoire de leurs maladies.</p>
+</body>
+</html>
