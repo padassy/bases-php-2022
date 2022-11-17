509ceb14d1748f394b27554157a159c2f07fdfcf
Michael J. Pitz <michaeljpitz@gmail.com>
2022-11-17 14:25:37 +0000

    test array

--- 06-array.php
+++ 06-array.php
@@ -22,12 +22,14 @@ $tab = [
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Array</title>
+    <title>Les Array</title>
 </head>
+
 <body>
     <h1>Array</h1>
     <p>Les tableaux sont la base de gestion des données en PHP comme dans la plupart des langages, on pourra également utiliser des objets, des fichier json ou xml pour faire la même chose, mais en PHP le tableau restera le principal format pour gérer toutes les données</p>
@@ -46,12 +48,12 @@ $tab = []; // manière courte
 print_r($tab);
 ?>
     </code></pre>
-<p>Pour afficher les valeurs manuellement d'un tableau indexé, on utilise le nom de la variable avec des $var[clef]</p>
-<pre><code>
+    <p>Pour afficher les valeurs manuellement d'un tableau indexé, on utilise le nom de la variable avec des $var[clef]</p>
+    <pre><code>
 echo $tab[2]." ".$tab[5][1]." ".$tab[5][2]." ".$tab[5][3]." <br>" donne :
 
-<?=$tab[2]." ".$tab[5][1]." ".$tab[5][2]." ".$tab[5][3]." <br>";?>
-<?=$tab[5][4][1]." ".$tab[5][1]." ".$tab[5][2]." ".$tab[5][3]." <br>";?>
+<?= $tab[2] . " " . $tab[5][1] . " " . $tab[5][2] . " " . $tab[5][3] . " <br>"; ?>
+<?= $tab[5][4][1] . " " . $tab[5][1] . " " . $tab[5][2] . " " . $tab[5][3] . " <br>"; ?>
 
 <strong>Moi ce que je veux c'est (exe2) :</strong>
 
@@ -67,25 +69,26 @@ Yes, je vais bien!
 
 
 </code></pre>
-<h3>V1</h3>
-<?=$tab[2]." ".$tab[5][1]." ".$tab[5][2]." ".$tab[5][3]." <br>";?>
-<?=$tab[5][4][1].", ".$tab[5][4][2]." <br>";?>
-<h3>V2</h3>
-<?=ucfirst($tab[2])." ".$tab[5][1]." ".$tab[5][2]." ".$tab[5][3]." <br>";?>
-<?=ucfirst($tab[5][4][1]).", ".$tab[5][4][2]." <br>";?>
+    <h3>V1</h3>
+    <?= $tab[2] . " " . $tab[5][1] . " " . $tab[5][2] . " " . $tab[5][3] . " <br>"; ?>
+    <?= $tab[5][4][1] . ", " . $tab[5][4][2] . " <br>"; ?>
+    <h3>V2</h3>
+    <?= ucfirst($tab[2]) . " " . $tab[5][1] . " " . $tab[5][2] . " " . $tab[5][3] . " <br>"; ?>
+    <?= ucfirst($tab[5][4][1]) . ", " . $tab[5][4][2] . " <br>"; ?>
 
-<h2>Le tableau associatif</h2>
-<p>La principale différence avecle tableau indexé est que l'on modifie au moins une clef, avec un numérique ou chaîne de caractère!</p>
-<?php
-$tab = array(2 => "un", "deux","trois");
-var_dump($tab);
-$tabAssoc = [
-    "id"=>29,
-    "prenom"=>"Yanis",
-    'nom'=> "Goodlife",
-    "mail"=> "mail@mail.com",
-];
-var_dump($tabAssoc);
-?>
+    <h2>Le tableau associatif</h2>
+    <p>La principale différence avecle tableau indexé est que l'on modifie au moins une clef, avec un numérique ou chaîne de caractère!</p>
+    <?php
+    $tab = array(2 => "un", "deux", "trois");
+    var_dump($tab);
+    $tabAssoc = [
+        "id" => 29,
+        "prenom" => "Yanis",
+        'nom' => "Goodlife",
+        "mail" => "mail@mail.com",
+    ];
+    var_dump($tabAssoc);
+    ?>
 </body>
+
 </html>
