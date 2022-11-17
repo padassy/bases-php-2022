eee421a0c0dc8ab80a8d6ba231ec9675955f4510
Jaouad1060 <jaouad1060@hotmail.com>
2022-11-17 11:20:45 +0100

    jaouad25

---
+++ 25-jaouad-foot/pages/ldc.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LDC</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body id="ldc">
+    
+    <?php include "incl/menu.php"; ?>
+    <h1 class="ldc1">Ligue des champions </h1>
+<p class="text1">
+    L'idée d'une coupe d'Europe interclubs est née dans l'esprit de journalistes parisiens ; les chocs Wolverhampton Wanderers – Honvéd Budapest puis Wolverhampton Wanderers – Spartak Moscou de décembre 1954 achèvent de les convaincre3. En effet, après les victoires des Wolves, le Daily Mail proclame le club « champion du monde des clubs ». Gabriel Hanot réplique dans L'Équipe en lançant un appel à la fondation d'une coupe d'Europe : une série d'articles du quotidien sportif parisien explique des semaines durant les avantages d'une telle épreuve, et les premières réactions sont plutôt positives4. Dès le 16 décembre 1954, Jacques de Ryswick signe un article présentant le « projet de coupe d'Europe interclubs ». Devant les réactions positives de l'Europe entière, L'Équipe rédige le 25 janvier 1955 un avant-projet de règlement signé par Jacques Ferran. Le 3 février 1955, le quotidien publie la liste des clubs invités à disputer la première édition de l'épreuve, et durant le mois de février, les clubs confirment leur participation3. Le 26 février 1955, la FIFA contacte L'Équipe pour lui confirmer que ses statuts n'empêchent pas l'organisation d'une telle compétition : « L'organisation d'un pareil tournoi n'est pas subordonnée à l'autorisation préalable de la FIFA, dont les statuts (art. 38) ne visent que les compétitions entre équipes représentatives nationales »5. L'UEFA vient à peine d'émerger des limbes, et ses statuts sont quasi vierges. Le 1er mars, le comité exécutif de l'UEFA se déclare inapte à assurer correctement l'organisation d'une telle épreuve et laisse à chaque fédération le libre choix d'accepter ou pas de prendre part à cette épreuve. On se retrousse alors les manches à L'Équipe en s'occupant d'aller démarcher les fédérations. La FFF se laisse finalement convaincre malgré la délicate question de surcharge du calendrier. Chaque fédération doit désigner son représentant et la plupart d'entre elles ne désignent pas le champion en titre mais font un choix par popularité du club à condition que celui-ci ait déjà remporté le championnat national au moins une fois6,7. Les 2 et 3 avril, L'Équipe réunit les dirigeants des clubs participants à Paris pour définir les dates de la compétition et leur faire approuver le règlement7.<br>
+
+<br>Revirement de situation le 8 mai, alors que tout est bouclé avec seize clubs partants (la désignation arbitraire des huitièmes de finale a même déjà eu lieu8). La FIFA sent le danger de laisser à des intervenants extérieurs la prise en main de compétitions et pousse finalement l'UEFA à prendre en charge l'organisation de l'épreuve3. La FIFA interdit même l'utilisation du mot « Europe » dans le nom de l'épreuve désirant réserver ce terme aux compétitions entre équipes nationales3. L'UEFA et la FIFA font le maximum pour décider les Anglais à participer mais la FA reste inflexible. Le Chelsea FC est pourtant partant9; mais son forfait est rendu officiel le 26 juillet 19553.</p>
+</body>
+</html>
---
+++ 25-jaouad-foot/pages/messi.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GOAT</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body id="messi">
+
+    <?php include "incl/menu.php"; ?>
+    <h2 class="messi1">Biographie</h2>
+   
+   <p class="messi1"><br>Lionel Messi naît le 24 juin 1987 à Rosario en Argentine. Dès l’âge de 5 ans, il joue au football dans un petit club de sa ville. Très talentueux, le jeune garçon rencontre pourtant de sérieux obstacles lorsqu’il connaît des problèmes de santé. Une maladie hormonale l’empêche de grandir et compromet sa possible carrière. Après deux années de traitement, il a suffisamment grandi et intègre l’équipe des jeunes de Barcelone.<br>
+   
+   <br>Il rejoint par la suite le FC Barcelona et devient très vite indispensable au club. Malgré son mètre 69, il dribble comme personne et son talent éblouit les supporters. Auteur de 400 buts, Lionel Messi est le seul joueur du monde à être quintuple ballon d’or et il est le premier buteur de l’Argentine. Le recordman parvient à mener son équipe en finale de la Coupe du monde 2014 face à la Mannschaft. La reconnaissance du footballeur est telle qu’un néologisme emprunté à son nom a intégré le dictionnaire espagnol; imessionante. L’adjectif désigne la manière extraordinaire de l’attaquant de jouer au foot. En août 2015, il est élu Meilleur joueur européen par l'UEFA. En janvier 2016, il remporte le Ballon d'or 2015.<br>
+   
+   <br>Le 5 août 2021, le club du FC Barcelone officialise le non-renouvellement du contrat de Lionel Messi. Quelques jours plus tard, le 10 août 2021, le joueur signe avec le PSG. Le 29 novembre 2021, il remporte le Ballon d'Or 2021, le septième de sa carrière.<br>
+   
+   <br>Côté vie privée, Lionel Messi est père de deux garçons, Thiago (né en novembre 2012) et Matteo (né en septembre 2015) avec sa compagne Antonella Roccuzzo. Le 30 juin 2017, le couple se marie en Argentine. En octobre 2017, le couple révèle attendre un troisième enfant. Le 10 mars 2018, la famille s'agrandit avec la naissance d'un troisième garçon prénommé Ciro.</p><br>
+   
+   <h2 class="messi1">Palmares</h2>
+   
+   
+   <p class="messi1">
+   <br>FIFA Ballon d’Or/Ballon d’Or : 2009, 2010, 2011, 2012, 2012, 2015.<br>
+   <br>Soulier d’or européen : 2009–10, 2011–12, 2012–13, 2016–17, 2017–18, 2018–19<br>
+   <br>Joueur Mondial de la FIFA de l’Année : 2009<br>
+   <br>Coupe du Monde de la FIFA, Ballon d’or : 2014<br>
+   <br>Ballon d’or de la Copa América : 2015<br>
+   <br>Prix du Joueur de l’année de l’UEFA : 2011, 2015<br>
+   <br>Footballeur de clubs de l’année de l’UEFA : 2009<br>
+   <br>Meilleur attaquant de club de l’année de l’UEFA : 2009<br>
+   <br>Onze d’Or : 2009, 2010-11, 2011-12, 2017-18<br>
+   <br>Globe Soccer Awards Meilleur joueur de l’année : 2015<br>
+   <br>Meilleur joueur de la Liga : 2008-09, 2009-10, 2010-10, 2010-11, 2011-12, 2012-13, 2014-15, 2014-15, 2017-18<br>
+   <br>Meilleur attaquant de la Liga : 2008-09, 2009-10, 2009-10, 2010-11, 2011-12, 2012-13, 2014-15, 2014-15, 2015-16<br>
+   <br>Coupe du Monde des Clubs de la FIFA – Ballon d’or : 2009, 2011<br>
+   <br>Trophée Pichichi : 2009-10, 2011-12, 2012-13, 2012-13, 2016-17, 2017-18, 2018-19<br>
+   <br>La finale de la Coupe du Monde des Clubs de la FIFA : 2009, 2011<br>
+   <br>Olimpia de Plata (footballeur argentin de l’année) : 2005, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2015, 2016, 2016, 2017<br>
+   <br>Meilleur Jeune Joueur de la Copa América : 2007<br>
+   <br>Jeune Joueur Mondial de l’Année FIFPro : 2006, 2007, 2008<br>
+   <br>Golden Boy (Jeune footballeur européen de l’année) : 2005<br>
+   <br>Joueur du mois en Liga : Janvier 2016, avril 2017, avril 2018, septembre 2018, mars 2019<br>
+   <br>FIFA FIFPro World XI : 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018<br>
+   <br>Équipe de l’année de l’UEFA : 2008, 2009, 2010, 2011, 2012, 2014, 2015, 2016, 2017, 2018<br>
+   <br>L’équipe de Liga de la saison : 2014-15, 2015-16<br>
+   <br>Équipe de la saison de l’UEFA La Liga : 2015-16, 2016-17<br>
+   <br>Équipe de rêve de la Coupe du Monde de la FIFA : 2014<br>
+   <br>Copa América Dream Team : 2007, 2011, 2015, 2016<br></p>
+</body>
+</html>
