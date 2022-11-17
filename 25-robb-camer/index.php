6fe2ed58661ad6918dad6cfb90d1ecf57afb2c99
Robb04 <cyrillerobbins@gmail.com>
2022-11-17 14:20:25 +0100

    le bonne exe2

--- 25-robb-camer/index.php
+++ 25-robb-camer/index.php
@@ -8,33 +8,33 @@ if(isset($_GET['pg'])){
   
       switch($_GET['pg']){
          case '':
-        include_once 'acceuil.php';
+        include_once 'pg/acceuil.php';
           break; 
         case 'geo':
-            include_once 'geographie.php';
+            include_once 'pg/geographie.php';
             break;
         case 'hist':
-            include_once 'histoire.php';
+            include_once 'pg/histoire.php';
             break;
         case 'cult':
-            include_once 'culture.php';
+            include_once 'pg/culture.php';
             break;
         case 'gal':
-            include_once 'galerie.php';
+            include_once 'pg/galerie.php';
             break;
          case 'spor':
-                include_once 'sport.php';
+                include_once 'pg/sport.php';
                 break;
         case 'form':
-            include_once 'contact.php';
+            include_once 'pg/contact.php';
             break;
           
         default:
-            include_once 'acceuil.php';
+            include_once 'pg/acceuil.php';
     }
 
 }else {
-    include_once 'acceuil.php';
+    include_once 'pg/acceuil.php';
 }
 
 ?>
