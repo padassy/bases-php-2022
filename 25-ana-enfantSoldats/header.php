39a6be6ed288e7ce5cb4afbb25ebe761d3f9f248
AnamariaTi <tirdeaana@outlook.fr>
2022-11-17 11:46:14 +0100

    exercice CF

---
+++ 25-ana-enfantSoldats/header.php
@@ -0,0 +1,5 @@
+
+<div class="header">
+<h1>Enfants soldats</h1>
+<img src="img/embleme.webp">
+</div>
---
+++ 25-ana-enfantSoldats/pg/galerie.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Enfants Soldats">
+    <meta name="keyword" content="Galerie">
+    <meta name="author" content="Ana-Maria">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Galerie</title>
+</head>
+<body>
+    <header>
+</header>
+
+<main>
+    <h2>Galerie</h2>
+
+    <div class="galerie" >
+        <img src="img/01.jpg" alt="enfant soldats">
+        <img src="img/02.jpg" alt="enfant soldats">
+        <img src="img/03.jpg" alt="enfant soldats">
+        <img src="img/06.jpg" alt="enfant soldats">
+        <img src="img/07.jpg" alt="enfant soldats">
+        <img src="img/08.jpg" alt="enfant soldats">
+    </div>
+
+</main>
+<footer>
+</footer>
+</body>
+</html>
