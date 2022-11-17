ca1854825dcc7f86cf09a528ad2cf86a946b601b
flow3flow <floramine75@hotmail.com>
2022-11-17 11:52:13 +0100

    excommit

--- 25-flow-paris/index.php
+++ 25-flow-paris/index.php
@@ -2,21 +2,30 @@
 
 if(isset($_GET['pg'])){
     switch($_GET['pg']){
-        case 'source':
-            include "pages/source.php";
+        case 'sacrecoeur':
+            include "pages/sacre-coeur.php";
             break;
-        case 'histoireetgeographie':
-            include "pages/histoireetgeographie.php";
-            break;
-        case 'cuisine':
-            include 'pages/cuisine.php';
-            break;
-        case 'accueuil':
-            include 'pages/accueil.php';
-            break;
+        case 'tour':
+                include "pages/tour-eiffel.php";
+                break;
+        case 'musee':
+                include "pages/musee-du-louvre.php";
+                    break;       
+        case 'lesinvalides':
+                include "pages/les-invalides.php";
+                    break;
+        case 'opera':
+                include "pages/opera-garnier.php";
+                    break;
+        case 'arc':
+            include "pages/arc-de-triomphe.php";
+                break;
+        case 'notredame':
+            include 'pages/notre-dame-de-paris.php';
+                break;
         default:
-            include_once "pages/accueil.php";
+            include_once "pages/ici-c-paris.php";
     }
 }else{
-    include_once "pages/accueil.php";
+    include_once "pages/ici-c-paris.php";
 }
