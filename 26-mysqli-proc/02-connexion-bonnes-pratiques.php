a973763dd0b3cbd500d192c7382574b2beffbec5
mikhawa <michaeljpitz@gmail.com>
2022-11-18 12:17:08 +0100

    try catch

--- 26-mysqli-proc/02-connexion-bonnes-pratiques.php
+++ 26-mysqli-proc/02-connexion-bonnes-pratiques.php
@@ -1,34 +1,55 @@
+<?php
+// Nos paramètres de connexion
+require_once "config.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion avec mysqli</title>
+    <title>Connexion avec mysqli bonnes pratiques</title>
 </head>
 <body>
     <h1>Connexion avec mysqli</h1>
     <h2>Méthode procédurale</h2>
-    <h3>mysqli_connect</h3>
+    <h3>mysqli_connect avec un try catch</h3>
     <p>Voir la page sur <a href="https://www.php.net/manual/fr/mysqli.construct.php" target="_blank">php.net</a></p>
     <code><pre>
-$mysqliConnect = mysqli_connect(
-    "localhost", // hôte
-    "root", // utilisateur
-    "", // mot de passe
-    "statistiques", // nom de la DB
-    3306, // port (localhost:3306)
-    // socket (optionnel)
-    );
+// Essai de connexion    
+try {
+    
+    // connexion à la DB mysql "statistiques"
+    $mysqliConnect = mysqli_connect(DB_HOST, DB_LOGIN, DB_PWD, DB_NAME, DB_PORT);
 
-var_dump($mysqliConnect);
+    // application du charset à notre connexion
+    mysqli_set_charset($mysqliConnect, DB_CHARSET);
+
+// capture de l'erreur plutôt que son affichage immédiat
+}catch(Exception $e){
+
+    // affichage de l'erreur encodée en utf8 avec utf8_encode()
+    echo utf8_encode($e->getMessage());
+}
     </pre></code>
-    <h4>Connexion</h4>
+    <h4>Connexion avec try catch</h4>
     <p>Connexion avec un var_dump pour tester notre connexion</p>
     <?php
+// Essai de connexion    
+try {
     // connexion à la DB mysql "statistiques"
-    $mysqliConnect = mysqli_connect("localhost","root","","statistiques",3306);
-    var_dump($mysqliConnect);
+    $mysqliConnect = mysqli_connect(DB_HOST, DB_LOGIN, DB_PWD, DB_NAME, DB_PORT);
+
+    // application du charset à notre connexion
+    mysqli_set_charset($mysqliConnect, DB_CHARSET);
+
+// capture de l'erreur plutôt que son affichage immédiat
+}catch(Exception $e){
+
+    // affichage de l'erreur encodée en utf8 avec utf8_encode()
+    echo utf8_encode($e->getMessage());
+}
+    //var_dump($mysqliConnect);
     ?>
     <h4>Déconnexion</h4>
     <p>Optionnel mais doit être mis pour les bonnes pratiques.<br>En effet en mysql ou mariaDB les connexions se ferment automatiquement en fin de script. ! En cas de connexion permanente, il ne faut pas l'utiliser!<br>Les connexions permanentes sont utilisées sur les gros serveurs ayant les capacités de les supporter (gain de vitesse)</p>
--- 26-mysqli-proc/README.md
+++ 26-mysqli-proc/README.md
@@ -11,12 +11,19 @@ https://www.php.net/manual/fr/mysqlinfo.api.choosing.php
 Car :
 
 ### PDO :
+
 Interface orientée objet -> Oui
 Interface procédurale->Non
 
 ### mysqli :
+
 Interface orientée objet -> Oui
 Interface procédurale-> **Oui**
 
+## Création d'un .gitignore
+
+On crée un .gitignore pour ne pas envoyer notre fichier config.php sur github pour des questions de sécurité. 
+
+config.php.create en est une copie sans les vrais paramètres de connexion !
 
 
