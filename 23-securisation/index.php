f119948073a19f46b550f5d2d959b81eaddf3059
mikhawa <michaeljpitz@gmail.com>
2022-11-18 09:52:28 +0100

    25-exe

--- 23-securisation/index.php
+++ 23-securisation/index.php
@@ -23,14 +23,18 @@ Routage (router), ou liens vers d'autres controleurs
 
 */
 if(isset($_GET['page'])){
+
     // si le nom de la variable get existe dans le tableau NOM_DE_PAGES
     if(in_array($_GET['page'],NOM_DE_PAGES)){
+
         // on charge la page (qui doit exister !)
         include "pages/".$_GET['page'].".php";
+        
     }else{
         include "pages/404.php";
     }
 }else{
     include "pages/homepage.php";
 }
+
 
