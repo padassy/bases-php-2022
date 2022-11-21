763120ac513e8d65f0a5c2c6539d9bdb905a877a
mikhawa <michaeljpitz@gmail.com>
2022-11-21 11:19:54 +0100

    first query with résultat

--- 26-mysqli-proc/03-premiers-query.php
+++ 26-mysqli-proc/03-premiers-query.php
@@ -19,10 +19,15 @@ try {
 }
 
 // création d'une variable contenant du string, qui est en réalité du SQL
-$sql = "SELECT * FROM `statistiques` ORDER BY `nom` ASC ;";
+$sql = "SELECT * FROM `statistiques` ORDER BY `nom`  ASC ;";
 
-// exécution de la requête avec mysqli_query
-$query = mysqli_query($mysqliConnect,$sql);
+
+// exécution de la requête avec mysqli_query, en cas d'échec, on arrête le script et on affiche l'erreur avec mysqli_error() et son numéro avec mysqli_erno
+$query = mysqli_query($mysqliConnect,$sql) or die("Erreur numéro ".mysqli_errno($mysqliConnect)." lors de la requête :".mysqli_error($mysqliConnect));
+
+// on vérifie combien de résultats on a obtenu avec notre select
+$nbPays = mysqli_num_rows($query);
+
 
 ?>
 <!DOCTYPE html>
@@ -35,7 +40,30 @@ $query = mysqli_query($mysqliConnect,$sql);
 </head>
 <body>
     <h1>Première requête</h1>
+    <h2>Nombre de pays <?=$nbPays?></h2>
+    <?php
+// si on a pas récupéré de pays (écriture sans {} dans l'html) 
+if(empty($nbPays)) :
 
+    // affichage de l'erreur sans utiliser echo (pratique pour un designe externe)
+    ?>
+    <h3>Pas de pays !</h3>
+    <?php
+// sinon (on a au moins un pays)
+else:
+
+    // tant qu'on a des lignes de résultats (pas de foreach car l'objet n'est pas traité à l'avance, utilisation de while) - mysqli_fetch_assoc convertit, ligne après ligne, le résultat en tableau associatif
+    while($item = mysqli_fetch_assoc($query)):
+
+        // affichage des pays par ordre alphabétique
+    ?>
+    <p><?=$item['nom']?> - <?=$item['population']?></p>
+    <?php
+    // fin de boucle
+    endwhile;
+// fin de if
+endif;
+    ?>
 </body>
 </html>
 <?php
