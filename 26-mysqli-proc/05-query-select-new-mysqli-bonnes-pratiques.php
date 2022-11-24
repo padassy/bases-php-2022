70752379f9783556e943f7b0060a356cb2f2cf1e
mikhawa <michaeljpitz@gmail.com>
2022-11-24 16:26:36 +0100

    mysqli ok

--- 26-mysqli-proc/05-query-select-new-mysqli-bonnes-pratiques.php
+++ 26-mysqli-proc/05-query-select-new-mysqli-bonnes-pratiques.php
@@ -3,25 +3,36 @@ require_once "config.php";
 
 require "functions.php";
 
-// connexion ancienne version
+// connexion avec try catch
 
+try {
     // création d'une variable contenant la connexion
-    $db = @mysqli_connect(DB_HOST,DB_LOGIN,DB_PWD,DB_NAME,DB_PORT);
+    $db = @mysqli_connect(DB_HOST, DB_LOGIN, DB_PWD, DB_NAME, DB_PORT);
 
     // si erreur
-    if(mysqli_connect_errno()){
+    if (mysqli_connect_errno()) {
         // sortie et affichage du numéro de l'erreur et son message
         die("Erreur numéro : ".mysqli_connect_errno()." Message : ".mysqli_connect_error());
     }
     // mettre le charset à la connexion
     mysqli_set_charset($db, DB_CHARSET);
+}catch(Exception $e){
 
+    die("Numéro : ".$e->getCode()." | ".$e->getMessage());
+
+}
 // sélectionne tout depuis la table stat ordonnée par nom ascendant
 $sql = "SELECT * FROM statistiques #WHERE ID =700 
         ORDER BY nom ASC";
 
-// requête sans try catch
-$query = mysqli_query($db,$sql) or die("Erreur numéro : ".mysqli_errno($db)." Message : ".mysqli_error($db));
+try {
+    // requête
+    $query = mysqli_query($db, $sql) or die("Erreur numéro : ".mysqli_errno($db)." Message : ".mysqli_error($db));
+}catch(Exception $e){
+
+    die("Numéro : ".$e->getCode()." | ".$e->getMessage());
+
+}
 
 // comptons le nombre de résultats
 $nb = mysqli_num_rows($query);
@@ -30,6 +41,7 @@ $nb = mysqli_num_rows($query);
 if(empty($nb)){
     // création du message
     $affiche = "Pas encore de pays dans notre programme";
+
 }else{
     // mettre les résultats dans une variable de type tableau pour faire un foreach dans la vue
     $datas = mysqli_fetch_all($query,MYSQLI_ASSOC);
