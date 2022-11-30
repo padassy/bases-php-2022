eeb0e3b791bf8f11436419b4c1ee82f710cbe005
mikhawa <michaeljpitz@gmail.com>
2022-11-30 16:34:35 +0100

    distanciel

--- 27-mysqli-suite/public/index.php
+++ 27-mysqli-suite/public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+//ini_set('display_errors', 1);
+
 /*
 
 DEPENDANCES ET CONNEXION
@@ -11,21 +13,19 @@ DEPENDANCES ET CONNEXION
 require_once "../config.php";
 
 // Essai de connexion à la DB
-try{
+try {
 
     // connexion
-    $connectDB = mysqli_connect(DB_HOST,DB_LOGIN,DB_PWD,DB_NAME,DB_PORT);
+    $connectDB = mysqli_connect(DB_HOST, DB_LOGIN, DB_PWD, DB_NAME, DB_PORT);
 
     // charset
-    mysqli_set_charset($connectDB,DB_CHARSET);
+    mysqli_set_charset($connectDB, DB_CHARSET);
 
-// si erreur, on l'attrape dans l'objet Exception, mis dans la variable $e (pointeur vers l'objet)
-}catch(Exception $e){
+    // si erreur, on l'attrape dans l'objet Exception, mis dans la variable $e (pointeur vers l'objet)
+} catch (Exception $e) {
 
     // arrêt du script et affichage du code et de l'erreur (en OO), utf8_encode permet d'éviter les caractères problématiques
-    die($e->getCode()." ".utf8_encode($e->getMessage()));
-    
-
+    die($e->getCode() . " " . utf8_encode($e->getMessage()));
 }
 
 
@@ -36,7 +36,7 @@ TENTATIVE D INSERTION
 */
 
 // si nos 2 variables POST sont présentes
-if(isset($_POST['titre'],$_POST['texte'])){
+if (isset($_POST['titre'], $_POST['texte'])) {
 
     // récupération des variables globales utilisateurs en locales
     $titre = $_POST['titre'];
@@ -44,7 +44,7 @@ if(isset($_POST['titre'],$_POST['texte'])){
 
     // on supprime les tags html susceptibles de poser problèmes
     $titre = strip_tags($titre);
-    $texte = strip_tags($texte,['br','p']);
+    $texte = strip_tags($texte, ['br', 'p']);
 
     // on retire les espaces avant et arrières
     $titre = trim($titre);
@@ -55,16 +55,16 @@ if(isset($_POST['titre'],$_POST['texte'])){
     $texte = htmlspecialchars($texte, ENT_QUOTES);
 
     // si, au final, aucun n'est vide
-    if(!empty($titre)&& !empty($texte)){
+    if (!empty($titre) && !empty($texte)) {
         // on prépare la requête
         $sql = "INSERT INTO `articles` (`titrearticles`, `textearticles`) VALUES ('$titre','$texte')";
         // on exécute la requête
-        $query = mysqli_query($connectDB,$sql) or die("Erreur : ".mysqli_error($connectDB));
+        $query = mysqli_query($connectDB, $sql) or die("Erreur : " . mysqli_error($connectDB));
     }
 
     //var_dump($_POST,$titre,$texte);
 
-    
+
 
 }
 
@@ -76,21 +76,22 @@ SELECTION DES ARTICLES
 */
 
 // requête
-$sql ="SELECT * FROM `articles` ORDER BY `datearticles` DESC";
+$sql = "SELECT * FROM `articles` ORDER BY `datearticles` DESC";
 
 // exécution de la requête
-$query = mysqli_query($connectDB,$sql) or die("Erreur :".mysqli_error($connectDB));
+$query = mysqli_query($connectDB, $sql) or die("Erreur :" . mysqli_error($connectDB));
 
 // on va compter le nombre de résultats
 $nb = mysqli_num_rows($query);
 
-// si pas de résultats
-if(empty($nb)){
+// si pas de résultats 
+if (empty($nb)) { // $nb => 0 voir https://www.php.net/manual/fr/function.empty.php
+
     // gestion de l'affichage de l'erreur
     $erreur0 = "Pas encore d'articles";
-}else{
+} else {
     // récupérer tous les articles dans un tableau indexé (chaque article est un tableau associatif -> MYSQLI_ASSOC)
-    $datas = mysqli_fetch_all($query,MYSQLI_ASSOC);
+    $datas = mysqli_fetch_all($query, MYSQLI_ASSOC);
 }
 
 /*
@@ -101,33 +102,39 @@ VUE DES ARTICLES
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nos articles</title>
 </head>
+
 <body>
     <h1>Nos articles</h1>
     <?php
     // pas d'article
-    if(isset($erreur0)):
+    if (isset($erreur0)) :
     ?>
-    <h2><?=$erreur0?></h2>
+        <h2><?= $erreur0; // écriture raccourcie pour echo 
+            ?></h2>
+
     <?php
     // au moins un article
-    else:
+    else :
     ?>
-        <h3><?= ($nb < 2)? "$nb article": "$nb articles"; // ternaire ?></h3>
+        <h3><?= ($nb < 2) ? "$nb article" : "$nb articles"; // ternaire 
+            ?></h3>
         <?php
-        // tant qu'on a des articles
-        foreach($datas as $item):
+        // tant qu'on a des articles, alias $item pour stocker la ligne en cours sous ce nom
+        foreach ($datas as $item) :
         ?>
-    <div class="article">
-        <h4><?=$item['titrearticles']?></h4>
-        <p><?=html_entity_decode($item['textearticles'],ENT_QUOTES); // pour réencoder les p et br?></p>
-        <p><?=$item['datearticles']?></p>
-    </div>
+            <div class="article">
+                <h4><?= $item['titrearticles'] ?></h4>
+                <p><?= html_entity_decode($item['textearticles'], ENT_QUOTES); // pour réencoder les p et br
+                    ?></p>
+                <p><?= $item['datearticles'] ?></p>
+            </div>
     <?php
         endforeach;
     endif;
@@ -137,7 +144,8 @@ VUE DES ARTICLES
     <form action="" method="POST" name="article">
         <input type="text" name='titre' placeholder="Le titre" required /><br>
         <textarea name='texte' placeholder="Votre texte" required></textarea><br>
-        <input type="submit" value="Envoyer"/>
+        <input type="submit" value="Envoyer" />
     </form>
 </body>
+
 </html>
